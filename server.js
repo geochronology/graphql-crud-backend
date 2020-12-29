c2a1b7103a67e8bb44e07350fab9c749d113122f
geochronology <54387644+geochronology@users.noreply.github.com>
2020-12-28 18:38:44 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+const cors = require('cors')
+const dotEnv = require('dotenv')
+
+// set env variables
+dotEnv.config()
+
+const app = express()
+
+// cors
+app.use(cors())
+
+// body parser middleware
+app.use(express.json())
+
+
+// gql
+const typeDefs = gql`
+  type Query {
+    greetings: String
+  }
+`
+
+const resolvers = {}
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+apolloServer.applyMiddleware({ app, path: '/graphql' })
+
+// server
+const PORT = process.env.PORT || 3000
+
+app.use('/', (req, res, next) => res.send({ message: 'hello' }))
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT: ${PORT}`);
+  console.log(`GraphQL endpoint: ${apolloServer.graphqlPath}`)
+})
+
+
